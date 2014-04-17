36e2b52e674195a93ddef6cd87de8771a7a6c1b1
Maziyar Boustani <maziyar_b4@yahoo.com>
2014-04-17 14:37:23 -0700

    #2 add a code that make wkt point vector data

---
+++ Vector/create_wkt_point.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+#Project:       Geothon (https://github.com/MBoustani/Geothon)
+#File:          Vector/create_wkt_point.py
+#Description:   This code create a wkt 3D point from latitude, longitue and elevation.
+#Author:        Maziyar Boustani (github.com/MBoustani)
+
+try:
+    import ogr
+except ImportError:
+    from osgeo import ogr
+
+latitude = 50
+longitude = 100
+#to make it 2D point
+elevation = 0
+
+point = ogr.Geometry(ogr.wkbPoint)
+point.AddPoint(latitude, longitude, elevation)
+point.ExportToWkt()
+print point
