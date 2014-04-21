1a634f9b87a081710acb01da4b206602cb471804
Maziyar Boustani <maziyar_b4@yahoo.com>
2014-04-21 10:31:07 -0700

    #4 A code that creates WKT format polygon data.

---
+++ Vector/create_wkt_polygon.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+'''
+Project:       Geothon (https://github.com/MBoustani/Geothon)
+File:          Vector/create_wkt_polygon.py
+Description:   This code create a wkt polygon from multi-points.
+Author:        Maziyar Boustani (github.com/MBoustani)
+'''
+
+try:
+    import ogr
+except ImportError:
+    from osgeo import ogr
+
+latitudes = [50, 51, 52, 53, 54]
+longitudes = [100, 110, 120, 130, 140]
+elevation = 0
+
+#Create a linear rign from series of points
+linear_ring = ogr.Geometry(ogr.wkbLinearRing)
+linear_ring.AddPoint(latitudes[0], longitudes[0], elevation)
+linear_ring.AddPoint(latitudes[1], longitudes[1], elevation)
+linear_ring.AddPoint(latitudes[2], longitudes[2], elevation)
+linear_ring.AddPoint(latitudes[3], longitudes[3], elevation)
+#last point should be same as first point to close the ring
+linear_ring.AddPoint(latitudes[4], longitudes[4], elevation)
+linear_ring.ExportToWkt()
+
+#Create the polygon from linear ring
+polygon = ogr.Geometry(ogr.wkbPolygon)
+polygon.AddGeometry(linear_ring)
+print polygon
